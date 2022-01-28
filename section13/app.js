eb45fe2d37a14e7b38c8dd10426a4d3f1e9d56b6
garywilki <wil20223@byui.edu>
2022-01-28 15:48:02 -0800

    section13

---
+++ section13/app.js
@@ -0,0 +1,73 @@
+const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
+
+const path = require('path');
+const PORT = process.env.PORT || 5000
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+//////////
+const corsOptions = {
+  origin: "https://cse-341-gw.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://nodeapp:jWJmU6pTT6OsZKZG@cluster0.8wyvu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//////////
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('61f47ce3a61c0e8f91c2b46d')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
